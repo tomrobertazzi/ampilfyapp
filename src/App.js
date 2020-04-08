2c881b18f2699b23a10b1223c2d6987a96597748
tomrobertazzi <62471807+tomrobertazzi@users.noreply.github.com>
2020-04-08 11:14:27 -0400

    Remove logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 
@@ -7,7 +7,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Welcome to 6B Partners!
         </p>
@@ -25,3 +24,4 @@ function App() {
 }
 
 export default App;
+//  <img src={logo} className="App-logo" alt="logo" />
